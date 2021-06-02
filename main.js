95c1081cb0a4bd735d80d6c3c79564976590742c
Anastasia <avbondarkova@mail.ru>
2021-06-02 15:49:05 +0300

    Task#1 and task#2

--- main.js
+++ main.js
@@ -17,3 +17,36 @@ const player2 = {
         console.log(this.name + ' fight');
     },
 };
+
+function createPlayer(playerClass, name, hp) {
+    const $arenas = document.querySelector('.arenas');
+
+    const $player = document.createElement('div');
+    const $progressbar = document.createElement('div');
+    const $character = document.createElement('div');
+    const $life = document.createElement('div');
+    const $name = document.createElement('div');
+    const $img = document.createElement('img');
+
+    $player.classList.add(playerClass);
+    $progressbar.classList.add('progressbar');
+    $character.classList.add('character');
+    $life.classList.add('life');
+    $name.classList.add('name');
+    
+    $img.src = 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif';
+    $name.innerHTML = 'SCORPION';
+
+    $player.appendChild($progressbar);
+    $player.appendChild($character);
+
+    $progressbar.appendChild($life);
+    $progressbar.appendChild($name);
+
+    $character.appendChild($img);
+
+    $arenas.appendChild($player);
+};
+
+createPlayer('player1', 'SCORPION', 50);
+createPlayer('player2', 'SUB-ZERO', 80);
