0552ce531f0984c280507e629a59992452e4f062
Anastasia <anastasia@Anastasias-MacBook-Air.local>
2021-06-02 15:45:24 +0300

    Task#0

---
+++ main.js
@@ -0,0 +1,19 @@
+const player1 = {
+    name: 'Scorpion',
+    hp: 100,
+    img: '',
+    weapon: ['blades', 'swords'],
+    attack: function() {
+        console.log(this.name + ' fight');
+    },
+};
+
+const player2 = {
+    name: 'Sub Zero',
+    hp: 100,
+    img: '',
+    weapon: ['frost', 'swords'],
+    attack: function() {
+        console.log(this.name + ' fight');
+    },
+};
