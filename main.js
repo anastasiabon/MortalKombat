1ae426cf92d8dc9d5aadd398765d7f3de08b80d5
Anastasia <avbondarkova@mail.ru>
2021-06-04 15:40:14 +0300

    Refactored main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const $arenas = document.querySelector('.arenas');
 
 const player1 = {
+    player: 1,
     name: 'SCORPION',
     hp: 77,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
@@ -11,6 +12,7 @@ const player1 = {
 };
 
 const player2 = {
+    player: 2,
     name: 'KITANA',
     hp: 85,
     img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
@@ -20,23 +22,27 @@ const player2 = {
     },
 };
 
-function createPlayer(playerClass, player) {
-    const $player = document.createElement('div');
-    const $progressbar = document.createElement('div');
-    const $character = document.createElement('div');
-    const $life = document.createElement('div');
-    const $name = document.createElement('div');
-    const $img = document.createElement('img');
+function createElement(tag, className) {
+    const $tag = document.createElement(tag);
 
-    $player.classList.add(playerClass);
-    $progressbar.classList.add('progressbar');
-    $character.classList.add('character');
-    $life.classList.add('life');
-    $name.classList.add('name');
+    if (className) {
+        $tag.classList.add(className);
+    }
+
+    return $tag;
+};
+
+function createPlayer(playerObj) {
+    const $player = createElement('div', 'player' + playerObj.player);
+    const $progressbar = createElement('div', 'progressbar');
+    const $character = createElement('div', 'character');
+    const $life = createElement('div', 'life');
+    const $name = createElement('div', 'name');
+    const $img = createElement('img');
     
-    $img.src = player.img;
-    $name.innerHTML = player.name;
-    $life.style.width = player.hp + '%';
+    $img.src = playerObj.img;
+    $name.innerHTML = playerObj.name;
+    $life.style.width = playerObj.hp + '%';
 
     $player.appendChild($progressbar);
     $player.appendChild($character);
@@ -46,8 +52,8 @@ function createPlayer(playerClass, player) {
 
     $character.appendChild($img);
 
-    $arenas.appendChild($player);
+    return $player;
 };
 
-createPlayer('player1', player1);
-createPlayer('player2', player2);
+$arenas.appendChild(createPlayer(player1));
+$arenas.appendChild(createPlayer(player2));
